8e80d7a9fb870b124137e46df3d5cfb6e18328c6
Hyun <eric0909kim@gmail.com>
2025-03-05 19:01:47 -0500

    Created user service for handling user logic

---
+++ src/main/java/com/eric/job_scraper/service/UserService.java
@@ -0,0 +1,28 @@
+package com.eric.job_scraper.service;
+
+import com.eric.job_scraper.model.User;
+import com.eric.job_scraper.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+}
