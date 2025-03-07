61c4b234a4df2dff499fcce76a6c7a5c0dcc4457
Hyun <eric0909kim@gmail.com>
2025-03-07 14:07:46 -0500

    Implemented postgresql connection

--- src/main/java/com/eric/job_scraper/JobScraperApplication.java
+++ src/main/java/com/eric/job_scraper/JobScraperApplication.java
@@ -1,5 +1,6 @@
 package com.eric.job_scraper;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobScraperApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(JobScraperApplication.class, args);
 	}
 
