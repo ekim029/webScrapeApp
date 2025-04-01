18ac58bb66e934c871f9dbeb4cd8e21859e4dea4
Hyun <eric0909kim@gmail.com>
2025-04-01 09:35:19 -0400

    edited parseJobs to include desired keywords

--- src/main/java/com/eric/job_scraper/service/scraping/JobParserService.java
+++ src/main/java/com/eric/job_scraper/service/scraping/JobParserService.java
@@ -1,17 +1,33 @@
 package com.eric.job_scraper.service.scraping;
 
+import com.eric.job_scraper.model.Preference;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.*;
+import java.util.stream.Collectors;
 
 @Service
 public class JobParserService {
 
-    public Map<String, List<String>> parseJob(Map<String, String> scrapedData) {
+    public Map<String, List<String>> parseJob(Map<String, String> scrapedData, Preference preference) {
         Map<String, List<String>> jobsBySite = new HashMap<>();
+
+        List<String> desiredKeywords = preference.getDesiredKeywords();
+        Pattern desiredPattern;
+        if (desiredKeywords == null || desiredKeywords.isEmpty()) {
+            desiredPattern = Pattern.compile(".*");
+        } else {
+            String stringRegex = desiredKeywords.stream()
+                    .map(keyword -> Pattern.quote(keyword))
+                    .collect(Collectors.joining("|"));
+
+            desiredPattern = Pattern.compile(stringRegex);
+        }
+
         for (String site : scrapedData.keySet()) {
             String data = scrapedData.get(site);
 
