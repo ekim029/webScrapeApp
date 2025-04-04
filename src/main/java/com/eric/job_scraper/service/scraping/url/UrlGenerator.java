2c936e791c5f2ca43ca8cb154cf1354bb23e2c49
Hyun <eric0909kim@gmail.com>
2025-04-04 13:43:54 -0400

    Shifting approach from web scraping to API-based

--- src/main/java/com/eric/job_scraper/service/scraping/url/UrlGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.eric.job_scraper.service.scraping.url;
-
-import com.eric.job_scraper.model.Preference;
-
-public interface UrlGenerator {
-    String generateUrl(Preference preference);
-}
