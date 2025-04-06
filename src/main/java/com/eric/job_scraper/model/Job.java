1b0cc6b7220af45ba7f740e3392ad3e6dc9e8e68
Hyun <eric0909kim@gmail.com>
2025-04-06 19:20:13 -0400

    Updated job class with lombok and a description field

--- src/main/java/com/eric/job_scraper/model/Job.java
+++ src/main/java/com/eric/job_scraper/model/Job.java
@@ -1,31 +1,16 @@
 package com.eric.job_scraper.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Job {
     private String jobTitle;
     private String company;
     private String location;
     private String url;
-
-    public Job(String jobTitle, String company, String location, String url) {
-        this.jobTitle = jobTitle;
-        this.company = company;
-        this.location = location;
-        this.url = url;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getUrl() {
-        return url;
-    }
+    private String description;
 }
